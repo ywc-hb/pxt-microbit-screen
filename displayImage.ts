4a93ad13d55720a16e7cdde608bf9198d4f82296
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-02 23:58:06 +0100

    Différents tests

--- displayImage.ts
+++ displayImage.ts
@@ -1,6 +1,6 @@
 function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
     list_colors: number[][], palette_colors: number[], width: number, height: number, 
-    nombre_occurence: number, replace_color: number) {
+    nombre_occurence: number, replace_color: number, transparency: boolean) {
     /*
     Explication des arguments : 
         x_depart, y_depart --> coordonnée de l'image (voir avec taille pixel)
@@ -11,6 +11,7 @@ function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
         height --> hauteur de l'image
         nombre_occurence --> nombre de pixel de l'image
         replace_color --> couleur à afficher à la place de la couleur de transparence
+        transparency --> true: affiche la couleur contenue dans replace_color
     */
 
     //Définition de la taille de chaque pixel (via fonction DOT_PIXEL)
@@ -40,9 +41,16 @@ function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
                 palette_colors[list_colors[Math.trunc(i / 140)][i % 140]], affiche_pixel);
         }
 
-        else if(replace_color != -1) {
-            LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                palette_colors[0], affiche_pixel);
+        else if(!transparency) {
+            if(replace_color != -1) {
+                LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
+                    replace_color, affiche_pixel);
+            }
+
+            else {
+                LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
+                    palette_colors[0], affiche_pixel);
+            }
         }
     }
 
