3074dbd513dd13a898d22d8648cb8850956e3c46
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-02 22:56:36 +0100

    Nouveau convertisseur
    Ce nouveau convertisseur répond aux différentes demandes faites par la nouvelle gestion des images par cette extension

---
+++ image-microbit.py
@@ -0,0 +1,56 @@
+#Le fichier d'entrée doit s'appeler 'colors_in_RGB565.txt'
+#Les fichiers de sortie s'appeleront 'colors_in_decimal.txt' et 'caracteristique.txt'
+#Le fichier 'colors.txt' contient 2 listes : 
+#	- La liste de couleurs
+#	- La palette de correspondance des couleurs
+
+
+with open("colors_in_RGB565.txt", "r") as file:
+    chaine = file.read()
+
+transparency = input("Veuillez entrer la couleur de transparence de votre image")
+
+liste = chaine.strip("\n").split(", ")
+
+height = chaine.count("\n")+1 #Obtemption de la hauteur de l'image
+width = len(liste) // height #Calcul de la largeur de l'image
+
+palette_colors = []
+liste_temp = liste[:]
+
+for i in liste_temp:
+	try:
+		palette_colors.append(liste_temp[0])
+		liste_temp = list(filter(lambda a: a != liste_temp[0], liste_temp))
+	except IndexError:
+		break
+
+palette_colors.remove(transparency)
+palette_colors.append(transparency)
+
+list_colors = "[["
+for i in range(len(liste)):
+    if i % 100 == 0 and i != 0:
+        list_colors += "], ["
+
+    if i < len(liste)-1:
+        list_colors += str(palette_colors.index(liste[i])) + ", "
+
+    elif i == len(liste)-1:
+        list_colors += str(palette_colors.index(liste[i]))
+
+list_colors += "]]"
+list_colors = list_colors.replace(", ]", "]")
+
+fich = open("colors.txt", "w")
+fich.write(list_colors)
+fich.write("\n\n\n")
+fich.write(str(palette_colors))
+fich.close()
+
+fichier = open("caracteristiques.txt", "w")
+
+fichier.write("\nHauteur de l'image : " + str(height))
+fichier.write("\nLargeur de l'image : " + str(width))
+ 	 
+fichier.close()
