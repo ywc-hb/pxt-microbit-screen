ec7cf257459d29cbaaa9649a27f6b1d7093fcccd
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-05 14:34:10 +0100

    Update displayImage.ts
    L'ancienne fonction displayImage() a été renommée displayImageParameters(). Désormais, la nouvelle fonction pour afficher une
    image est displayImage('la fonction qui contient l'image', x, y, taille_pixel). Rafraîchir l'écran n'est plus nécessaire.

--- displayImage.ts
+++ displayImage.ts
@@ -1,11 +1,17 @@
-function displayImage(x_depart: number, y_depart: number, image: any, taille_pixel: number, 
-    transparency: boolean, replace_color: number) {
+function displayImageParameters(x_depart: number, y_depart: number, taille_pixel: number,
+    list_colors: number[][], palette_colors: number[], width: number, height: number,
+    nombre_occurence: number, replace_color: number, transparency: boolean) {
     /*
     Explication des arguments : 
         x_depart, y_depart --> coordonnée de l'image (voir avec taille pixel)
         taille_pixel --> taille de chaque pixel
-        transparency --> affiche la couleur contenue dans replace_color si 'true', sinon ne fait rien
-        replace_color --> couleur à afficher à la place de la couleur de transparence, la couleur par défaut si -1
+        list_colors --> liste des couleurs de chaque pixel de l'image
+        palette_colors --> palette de correspondance entre les indices (présents dans 'list_colors') et les couleurs en décimal
+        width --> largeur de l'image
+        height --> hauteur de l'image
+        nombre_occurence --> nombre de pixel de l'image
+        replace_color --> couleur à afficher à la place de la couleur de transparence
+        transparency --> true: affiche la couleur contenue dans replace_color
     */
 
     //Définition de la taille de chaque pixel (via fonction DOT_PIXEL)
@@ -29,26 +35,30 @@ function displayImage(x_depart: number, y_depart: number, image: any, taille_pix
     }
 
     //Affichage des points 1 par 1
-    for (let i = 0; i < image.numberOfOccurence; i++) {
-        if (image.listOfColors[Math.trunc(i / 100)][i % 100] != 0) {
-            LCD1IN8.DrawPoint(x_depart + (i % image.width) * taille_pixel, y_depart + Math.trunc(i / image.width) * taille_pixel,
-                image.paletteOfColors[image.listOfColors[Math.trunc(i / 100)][i % 100]], affiche_pixel);
+    for (let i = 0; i < nombre_occurence; i++) {
+        if (list_colors[Math.trunc(i / 100)][i % 100] != 0) {
+            LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
+                palette_colors[list_colors[Math.trunc(i / 100)][i % 100]], affiche_pixel);
         }
 
         else if (!transparency) {
             if (replace_color != -1) {
-                LCD1IN8.DrawPoint(x_depart + (i % image.width) * taille_pixel, y_depart + Math.trunc(i / image.width) * taille_pixel,
+                LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
                     replace_color, affiche_pixel);
             }
 
             else {
-                LCD1IN8.DrawPoint(x_depart + (i % image.width) * taille_pixel, y_depart + Math.trunc(i / image.width) * taille_pixel,
-                    image.paletteOfColors[0], affiche_pixel);
+                LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
+                    palette_colors[0], affiche_pixel);
             }
 
         }
     }
+    LCD1IN8.LCD_DisplayWindows(x_depart - taille_pixel, y_depart - taille_pixel, (width - 1) * taille_pixel + x_depart, (height - 1) * taille_pixel + y_depart)
 
-    //Rafraichissement de la partie de l'écran correspondant à la taille de l'image doit se faire en dehors de cette fonction
-    //test(x_depart - taille_pixel, y_depart - taille_pixel, (width - 1) * taille_pixel + 10, (height - 1) * taille_pixel + 10)
+} 
+function displayImage(picture: any, x: number, y: number, taille_pixel: number){
+    displayImageParameters(x, y, taille_pixel, picture.listOfColor, picture.paletteOfColors, picture.width, picture.height, picture.numberOfOccurence, 1, true);
+    delete picture.listOfColor
+    delete picture.paletteOfColors
 }
