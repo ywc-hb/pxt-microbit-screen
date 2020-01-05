c23f9a8f9573d3e059b6b5801b3d8bf492574eac
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-05 20:23:32 +0100

    Nouvelle gestion des images fonctionnelle

--- test.ts
+++ test.ts
@@ -3,24 +3,20 @@ LCD1IN8.LCD_Init();
 LCD1IN8.LCD_Clear();
 /*
 //Exemple d'affichage d'image
-
-displayImage(displayPikachuRun(), 10, 10, 2);
-
-displayImage(displayPikachuStay(), 70, 10);
-
-
-//Exemple d'affichage de dialogue
-
-let texte1 = "Coucou ! Comment ca vaaujourd'hui ?";
-displayDialogue(3, 75, texte1, 0, 75, true, true);
-
-let texte = "Coucou  tout le monde !";
-displayDialogue(100, 3, texte, 0, 75);*/
-
 displayImage(1, 1, 1, -1, true, displayPikachuRun())
 displayImage(100, 1, 1, -1, true, displayPikachuStay())
 displayImage(1, 40, 1, -1, true, displayPikachuRun())
 displayImage(100, 40, 1, -1, true, displayPikachuStay())
 displayImage(50, 1, 1, -1, true, displayPikachuRun())
 displayImage(50, 40, 1, -1, true, displayPikachuStay())
+*/
 
+let test = {
+    listOfColor: [[2, 2, 2, 2, 2], [1, 0, 1, 0, 1]],
+    paletteOfColors: [1, 64512], 
+    width: 10,
+    height: 1,
+    numberOfOccurence: 10
+};
+
+displayImage(50, 40, 2, test, false)
