2c91fef22690fa0a3bba269b60bed636d9cad0ec
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-05 18:42:05 +0100

    Optimisations diverses

--- displayImage.ts
+++ displayImage.ts
@@ -1,6 +1,6 @@
-function displayImageParameters(x_depart: number, y_depart: number, taille_pixel: number,
-    list_colors: number[][], palette_colors: number[], width: number, height: number,
-    nombre_occurence: number, replace_color: number, transparency: boolean) {
+function displayImage(x_depart: number, y_depart: number, taille_pixel: number,
+    replace_color: number, transparency: boolean, 
+    picture: {listOfColor: number[][], paletteOfColors: number[], width: number, height: number, numberOfOccurence: number}) {
     /*
     Explication des arguments : 
         x_depart, y_depart --> coordonnée de l'image (voir avec taille pixel)
@@ -35,33 +35,26 @@ function displayImageParameters(x_depart: number, y_depart: number, taille_pixel
     }
 
     //Affichage des points 1 par 1
-    for (let i = 0; i < nombre_occurence; i++) {
-        if (list_colors[Math.trunc(i / 100)][i % 100] != 0) {
-            LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                palette_colors[list_colors[Math.trunc(i / 100)][i % 100]], affiche_pixel);
+    for (let i = 0; i < picture.numberOfOccurence; i++) {
+        if (picture.listOfColor[Math.trunc(i / 100)][i % 100] != 0) {
+            LCD1IN8.DrawPoint(x_depart + (i % picture.width) * taille_pixel, y_depart + Math.trunc(i / picture.width) * taille_pixel,
+                picture.paletteOfColors[picture.listOfColor[Math.trunc(i / 100)][i % 100]], affiche_pixel);
         }
 
         else if (!transparency) {
             if (replace_color != -1) {
-                LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
+                LCD1IN8.DrawPoint(x_depart + (i % picture.width) * taille_pixel, y_depart + Math.trunc(i / picture.width) * taille_pixel,
                     replace_color, affiche_pixel);
             }
 
             else {
-                LCD1IN8.DrawPoint(x_depart + (i % width) * taille_pixel, y_depart + Math.trunc(i / width) * taille_pixel,
-                    palette_colors[0], affiche_pixel);
+                LCD1IN8.DrawPoint(x_depart + (i % picture.width) * taille_pixel, y_depart + Math.trunc(i / picture.width) * taille_pixel,
+                    picture.paletteOfColors[0], affiche_pixel);
             }
 
         }
     }
-    LCD1IN8.LCD_DisplayWindows(x_depart - taille_pixel, y_depart - taille_pixel, (width - 1) * taille_pixel + x_depart, (height - 1) * taille_pixel + y_depart)
-
-} 
-function displayImage(picture: any, x: number, y: number, taille_pixel: number = 1){
-    displayImageParameters(x, y, taille_pixel, picture.listOfColor, picture.paletteOfColors, picture.width, picture.height, picture.numberOfOccurence, -1, true);
+    LCD1IN8.LCD_DisplayWindows(x_depart - taille_pixel, y_depart - taille_pixel, (picture.width - 1) * taille_pixel + x_depart, (picture.height - 1) * taille_pixel + y_depart)
     delete picture.listOfColor
     delete picture.paletteOfColors
-//    delete picture.width
-//    delete picture.height
-//    delete picture.numberOfOccurence
 }
--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
 LCD1IN8.LCD_Init();
 LCD1IN8.LCD_Clear();
-
+/*
 //Exemple d'affichage d'image
 
 displayImage(displayPikachuRun(), 10, 10, 2);
@@ -15,4 +15,12 @@ let texte1 = "Coucou ! Comment ca vaaujourd'hui ?";
 displayDialogue(3, 75, texte1, 0, 75, true, true);
 
 let texte = "Coucou  tout le monde !";
-displayDialogue(100, 3, texte, 0, 75);
+displayDialogue(100, 3, texte, 0, 75);*/
+
+displayImage(1, 1, 1, -1, true, displayPikachuRun())
+displayImage(100, 1, 1, -1, true, displayPikachuStay())
+displayImage(1, 40, 1, -1, true, displayPikachuRun())
+displayImage(100, 40, 1, -1, true, displayPikachuStay())
+displayImage(50, 1, 1, -1, true, displayPikachuRun())
+displayImage(50, 40, 1, -1, true, displayPikachuStay())
+
